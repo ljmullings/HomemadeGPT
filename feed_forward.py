fa23cfbd7b494d7c635595cf71deee07d904bb3c
Laura Chan-Mullings <40129588+ljmullings@users.noreply.github.com>
2024-06-18 16:56:11 -0500

    Create feed_forward.py

---
+++ feed_forward.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class FeedForward(nn.Module):
+    def __init__(self, embed_size, forward_expansion):
+        super(FeedForward, self).__init__()
+        self.fc1 = nn.Linear(embed_size, forward_expansion * embed_size)
+        self.fc2 = nn.Linear(forward_expansion * embed_size, embed_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
